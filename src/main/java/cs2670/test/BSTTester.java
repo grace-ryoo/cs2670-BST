549d022e6cef8de21996d77db4cca95eea31d51e
Grace Ryoo <ger85847@uga.edu>
2024-03-11 15:01:17 -0400

    3-11:p3

--- src/main/java/cs2670/test/BSTTester.java
+++ src/main/java/cs2670/test/BSTTester.java
@@ -1,10 +1,16 @@
 package cs2670.test;
 
+import cs2670.search.BST;
+
 public class BSTTester {
 
     public static void main(String[] args) {
 
-
+        BST tree1 = new BST();
+        tree1.insert(3);
+        tree1.insert(12);
+        tree1.insert(1);
+        System.out.println(tree1);
 
 
 
